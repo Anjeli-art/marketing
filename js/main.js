5e7eae54456149540dfb3fd1698ed54366a89675
Anjeli-art <shinyanj@gmail.com>
2023-01-17 22:10:23 +0300

    fix: map path

--- js/main.js
+++ js/main.js
@@ -3,8 +3,8 @@ $(function () {
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
-      '<button type="button" class="slick-prev"><img src="./images/arrow-left.svg" alt="arrow left"></button>',
+      '<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="arrow left"></button>',
     nextArrow:
-      '<button type="button" class="slick-next"> <img src="./images/arrow-right.svg" alt="arrow right"></button>',
+      '<button type="button" class="slick-next"> <img src="../images/arrow-right.svg" alt="arrow right"></button>',
   });
 });
