c449142725de57f8cb4cc6e049964737f3583e84
Anjeli-art <shinyanj@gmail.com>
2023-01-21 21:11:05 +0300

    feat: add adaptive

--- js/main.js
+++ js/main.js
@@ -1,10 +1,30 @@
 $(function () {
-  var mixer = mixitup(".portfolio__content");
+  $(".menu__list a, .go-top").on("click", function (e) {
+    e.preventDefault();
+    var id = $(this).attr("href"),
+      top = $(id).offset().top;
+    $("body,html").animate({ scrollTop: top }, 2000);
+  });
+
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
       '<button type="button" class="slick-prev"><img src="../images/arrow-left.svg" alt="arrow left"></button>',
     nextArrow:
       '<button type="button" class="slick-next"> <img src="../images/arrow-right.svg" alt="arrow right"></button>',
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          arrows: false,
+        },
+      },
+    ],
   });
+
+  $(".menu__btn, .menu__list a").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
+
+  var mixer = mixitup(".portfolio__content");
 });
